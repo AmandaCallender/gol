31aab12a4f492ea267b9ade13eae6c6dd09e76f9
AmandaCallender <58009118+AmandaCallender@users.noreply.github.com>
2020-09-22 21:26:16 -0400

    Updates

---
+++ precache-manifest.54290ab2b02f9e8979bc242681823dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be7c8e02069d5bfd3ddf76b4e8abd3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99944ff2fcbb003a3499",
+    "url": "/static/css/main.39d2ddbb.chunk.css"
+  },
+  {
+    "revision": "a0c80b8abe7491a5471f",
+    "url": "/static/js/2.230cfa4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.230cfa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99944ff2fcbb003a3499",
+    "url": "/static/js/main.253ed5d1.chunk.js"
+  },
+  {
+    "revision": "d2c1a985bbc53c4ba8f8",
+    "url": "/static/js/runtime-main.13432be2.js"
+  },
+  {
+    "revision": "73b1728f82a1e502affb27809842b64f",
+    "url": "/static/media/background.73b1728f.jpg"
+  }
+]);
